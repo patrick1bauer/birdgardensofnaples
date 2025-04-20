1131b6edaf7331ebf8916574e193bdb91170d49d
Patrick Bauer <43837003+patrick1bauer@users.noreply.github.com>
2025-04-19 22:29:36 -0400

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="div-component">
       <div className="div-content">
         <div id="home-title">Bird Gardens of Naples</div>
-        <h1 id="home-description">Leo go brrrrrrrr lol</h1>
+        <h1 id="home-description">Leo go brrrrrrrr lol hi</h1>
         <img src="pictures/leo.webp" alt="Leo" />
       </div>
     </div>
